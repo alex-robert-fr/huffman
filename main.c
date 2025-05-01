0dfa9e517d0e5fa0967f39394a7753a2435a1bf7
Alex Robert <contact.alexrobert04@gmail.com>
2025-05-01 23:24:46 +0200

    ✨ Count size of file

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int main() {
-  printf("Hello, Huffman!\n");
+  int fd = open("input.txt", O_RDONLY);
+  if (fd < 0) {
+    return 1;
+  }
+  struct stat sb;
+  if (fstat(fd, &sb) < 0) {
+    close(fd);
+    return 1;
+  }
+  printf("File size: %lld bytes\n", (long long)sb.st_size);
   return 0;
 }
